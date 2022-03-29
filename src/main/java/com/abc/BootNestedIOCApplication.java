8c12dd9bb8a671639be7a6c928073d8c70298221
Nagarjuna Amboji <contactnagarjuna247@gmail.com>
2022-03-29 15:23:55 +0530

    Initial commit.

---
+++ src/main/java/com/abc/BootNestedIOCApplication.java
@@ -0,0 +1,11 @@
+package com.abc;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootNestedIOCApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
