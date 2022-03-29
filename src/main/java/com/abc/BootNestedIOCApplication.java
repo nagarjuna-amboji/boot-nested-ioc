c3731566a2a42c0ed44a564311904a27a021eb75
Nagarjuna Amboji <contactnagarjuna247@gmail.com>
2022-03-29 15:42:43 +0530

    Added Nested factory code to the repo.

--- src/main/java/com/abc/BootNestedIOCApplication.java
+++ src/main/java/com/abc/BootNestedIOCApplication.java
@@ -1,11 +1,28 @@
 package com.abc;
 
+import com.abc.bean.LaunchPad;
+import com.abc.config.ChildConfig;
+import com.abc.config.ParentConfig;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class BootNestedIOCApplication {
 
     public static void main(String[] args) {
+
+        SpringApplicationBuilder applicationBuilder = new SpringApplicationBuilder();
+        SpringApplication springApplication = applicationBuilder
+                .parent(ParentConfig.class)
+                .sources(ChildConfig.class)
+                .build();
+
+        ApplicationContext context = springApplication.run(args);
+        LaunchPad launchPad = context.getBean(LaunchPad.class);
+        launchPad.launch();
+
 
     }
 }
